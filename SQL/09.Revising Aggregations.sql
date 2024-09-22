223d8cd26a619b682518e773737bbd5d68ac595b
YukaKoshiba <146679300+YukaKoshiba@users.noreply.github.com>
2024-09-21 23:10:50 -0500

    Create 09.Revising Aggregations.sql

---
+++ SQL/09.Revising Aggregations.sql
@@ -0,0 +1,13 @@
+-- Language: MySQL
+
+-- Revising Aggregations - The Count Function
+SELECT COUNT(NAME) FROM CITY
+    WHERE POPULATION >= 100000;
+
+-- Revising Aggregations - The Sum Function
+SELECT SUM(POPULATION) FROM CITY
+    WHERE DISTRICT = 'California';
+
+-- Revising Aggregations - Averages
+SELECT AVG(POPULATION) FROM CITY
+    WHERE DISTRICT = 'California';
