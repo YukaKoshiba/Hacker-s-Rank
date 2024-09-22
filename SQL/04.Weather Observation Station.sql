48244c7c1cc164c498c1634bf684483ab2c9ee55
YukaKoshiba <146679300+YukaKoshiba@users.noreply.github.com>
2024-09-21 23:00:19 -0500

    Update 04.Weather Observation Station.sql
    Add other practices.

--- SQL/04.Weather Observation Station.sql
+++ SQL/04.Weather Observation Station.sql
@@ -3,6 +3,9 @@
 -- Weather Observation Station 1
 SELECT CITY, STATE FROM STATION;
 
+-- Weather Observation Station 2
+SELECT ROUND(SUM(LAT_N),2) AS lat, ROUND(SUM(LONG_W),2) AS lon FROM STATION;
+
 -- Weather Observation Station 3
 SELECT DISTINCT CITY FROM STATION
     WHERE MOD(ID,2)=0;
@@ -56,4 +59,29 @@ UNION
 
 -- Weather Observation Station 12
 SELECT DISTINCT CITY FROM STATION
-    WHERE LEFT(CITY,1) NOT IN ('a','i','u','e','o') and RIGHT(CITY,1) NOT IN ('a','i','u','e','o')
+    WHERE LEFT(CITY,1) NOT IN ('a','i','u','e','o') and RIGHT(CITY,1) NOT IN ('a','i','u','e','o');
+
+-- Weather Observation Station 13
+SELECT FLOOR(SUM(LAT_N)*10000)/10000 FROM STATION
+    WHERE LAT_N BETWEEN 38.7880 AND 137.2345 ;
+
+-- Weather Observation Station 14
+SELECT FLOOR(MAX(LAT_N)*10000)/10000 FROM STATION
+    WHERE LAT_N <137.2345;
+
+-- Weather Observation Station 15
+SELECT ROUND(LONG_W,4) FROM STATION
+    WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345);
+
+-- Weather Observation Station 16
+SELECT ROUND(MIN(LAT_N),4) FROM STATION
+    WHERE LAT_N > 38.7780;
+
+-- Weather Observation Station 17
+SELECT ROUND(LONG_W,4) FROM STATION
+    WHERE LAT_N =
+    (SELECT MIN(LAT_N) FROM STATION
+     WHERE LAT_N > 38.7780);
+
+-- Weather Observation Station 18
+SELECT ABS(ROUND(MIN(LAT_N)-MAX(LAT_N)+MIN(LONG_W)-MAX(LONG_W),4)) FROM STATION;
